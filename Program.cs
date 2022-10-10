7291f64924c4b1cf1c9a8b0e8071d1599e034377
IsmaelMassolo <ismaelmassolo@gmail.com>
2022-10-10 19:02:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*Solicite al usuario que ingrese su nombre completo y guárdelo
+en una variable del tipo correspondiente. Muestre por pantalla un
+saludo personalizado usando el nombre almacenado. Se debe esperar
+que el usuario presione una tecla para finalizar programa.*/
+using System;
+using System.Threading;
+namespace TpCSharpEjercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hola!");
+            Thread.Sleep (3000);
+            Console.Clear();
+            string entrada = "";
+            Console.WriteLine("A continuación ingrese su nombre completo");
+            entrada=Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Que tal {0}?!, que tengas buen día!", entrada);
+            
+        }
+    }
+}
